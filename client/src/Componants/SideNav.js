9eb48f8cc4c36dfa80aed16a19c5de21d19ffb27
Adam Wadsworth <adam_y2005@hotmail.com>
2022-12-13 01:03:42 +0000

    added side bar and buttons

--- client/src/Componants/SideNav.js
+++ client/src/Componants/SideNav.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "./SideNav.css";
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { faDiagramProject } from '@fortawesome/free-solid-svg-icons'
 
 const SideNav = () => {
     return(
-        <div className="sideNav">
-            <button>Test button</button>
+        <div className="sideNav">  
+            <ul className="nav_List">
+            <li> 
+                <FontAwesomeIcon icon={faDiagramProject} /> LOGO
+                </li>
+                <li> 
+                <FontAwesomeIcon icon={faUpload} /> Upload Zip File
+                </li>
+                <li> 
+                <FontAwesomeIcon icon={faDatabase} /> Show Variables
+                </li>
+                <i class="fa-solid fa-cabin"></i>
+
+            </ul>
+            
+           
 
         </div>
 
-    )
+    ) 
 
 
 }
