d511d60081ccca8449751aff1ab215a86f809111
Adam <adam_y2005@hotmail.com>
2022-12-14 23:26:13 +0000

    next

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,26 @@ import SideNav from './Componants/SideNav'
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((data) => setData(data));
+  }, []);
+    
+
+
   return (
     <div className="App">
       
       <SideNav></SideNav>
+      
       <Navbar></Navbar>
+      <div style={{width: '100px', height: '100px', background: 'green', order: 2}}>{data} 2</div>
+      <div style={{width: '100px', height: '100px', background: 'red', order: 2}}>{data}</div>
+      
+      
       
       
     </div>
