d79b4da6f28830dc0c0c3cd723f333dbdbe3a273
Adam Wadsworth <adam_y2005@hotmail.com>
2023-01-06 01:34:32 +0000

    swaped to simple code editor

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import TopBar from './Componants/TopBar';
 import SideNav from './Componants/SideNav'
 import Flow from './Componants/Flow'
+import Editor from 'react-simple-code-editor';
+import {highlight, languages} from 'prismjs'
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/themes/prism-tomorrow.css';
 import SyntaxHighlighter from "react-syntax-highlighter"
 import {a11yDark} from "react-syntax-highlighter/dist/esm/styles/hljs";
 import './App.css';
@@ -30,9 +36,11 @@ function App() {
 
       <div className="row Panels">
         <div className="col-8 jsonPanel">Flow JSON
-          <SyntaxHighlighter children={code} showLineNumbers={true} id="jsonText" language="JSON" style={a11yDark}>
+          {/* <SyntaxHighlighter children={code} showLineNumbers={true} id="jsonText" language="JSON" style={a11yDark}>
 
-          </SyntaxHighlighter>
+          </SyntaxHighlighter> */}
+
+          <Editor preClassName={'language-json'}  id="jsonText" value={code} onValueChange={code => setCode(code)} highlight={code => highlight(code, languages.json, 'json')} padding={10} style={{fontFamily: '"Fira code", "Fira Mono", monospace'}}></Editor>
         </div>
         <div className="col-4 infopanel">
           details
