0b220004d1a062f2539f3e5eadcf39936620ff86
Adam Wadsworth <Adam_y2005@hotmail.com>
2023-01-13 01:25:26 +0000

    Update 15 files

--- client/src/Componants/Uploadbutton.js
+++ client/src/Componants/Uploadbutton.js
@@ -1,61 +1,53 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 import { BiUpload } from "react-icons/bi";
 
 const Uploadbutton = (props) => {
-    const [zipfile, setzipfile] = useState(null);
+  const [zipfile, setzipfile] = useState(null);
 
+  const data = new FormData();
 
-    const data = new FormData()
-
-    const sendData = () => {
-
-        if (zipfile == null) {
-
-        } else {
-            data.append('file', zipfile)
-            axios.post("http://localhost:9000/testAPI", data, {
-            }).then(function (response) {
-                
-                
-                props.updateCode(JSON.stringify(response.data,undefined,4))
-            })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        }
+  const sendData = () => {
+    if (zipfile == null) {
+    } else {
+      data.append("file", zipfile);
+      axios
+        .post("/testAPI", data, {})
+        .then(function (response) {
+          props.updateCode(JSON.stringify(response.data, undefined, 4));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-    const handleChange = (e) => {
-        setzipfile(e.target.files[0])
-        
-       
-    }
+  };
+  const handleChange = (e) => {
+    setzipfile(e.target.files[0]);
+  };
 
-    useEffect(() => {
-        sendData();
+  useEffect(() => {
+    sendData();
+  });
 
-
-    })
-
-
-    return (
-        <div>
-            <li>
-                <a href="#">
-                    <label htmlFor="file-input" style={{ "display": "list-item" }} >
-                        <BiUpload className="react-icons"></BiUpload>
-                        <span className="links_name">Upload zip File</span>
-                        <input id="file-input" style={{ "display": "none" }} type="file" onChange={handleChange}></input>
-                    </label>
-                </a>
-                <span className="tooltips">Upload zip File</span>
-            </li>
-        </div>
-
-    )
-
+  return (
+    <div>
+      <li>
+        <a href="#">
+          <label htmlFor="file-input" style={{ display: "list-item" }}>
+            <BiUpload className="react-icons"></BiUpload>
+            <span className="links_name">Upload zip File</span>
+            <input
+              id="file-input"
+              style={{ display: "none" }}
+              type="file"
+              onChange={handleChange}
+            ></input>
+          </label>
+        </a>
+        <span className="tooltips">Upload zip File</span>
+      </li>
+    </div>
+  );
 };
 
-
-
-export default Uploadbutton
+export default Uploadbutton;
